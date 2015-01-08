c0a3d5f987c37129883d630d6c675935df8ce481
Wade Guidry <wguidry@pugetsound.edu>
2015-01-08 09:59:10 -0800

    Updates to script comments
    Updates to script comments

--- trending_c.php
+++ trending_c.php
@@ -1,23 +1,64 @@
 <?php
 
-/*
+/**************************************************
 
-Necessary components, and credits:
+January 8, 2015
 
-The source for the jquery and CSS techniques came from:
-http://sorgalla.com/jcarousel/ (https://github.com/jsor/jcarousel)
+Wade Guidry, University of Puget Sound, wguidry@pugetsound.edu
 
-The cover art comes from the following site:
-http://openlibrary.org
 
-*/
+Script for generating an HTML / CSS / javascript-based book cover carousel using Alma Analytics, the Analytics API, and some jquery and CSS.
+
+An example of the output of this script can currently be seen at:
+
+http://www.pugetsound.edu/academics/academic-resources/collins-memorial-library/new-resources/recently-read-collins/
+
+In this particular example, the script generates a carousel of books recently checked out (and already returned) at Collins Memorial Library.
+
+If you are an Ex Libris Alma customer, and have access to Alma Analytics, a copy of the Analytics report against which this script runs can be found in the Alma Analytics Community folder, at:
+
+	/Shared Folders/Community/Reports/University of Puget Sound/trending_titles
+	
+This script relies on the following additional resources:
+
+	1. JCarousel
+
+		jCarousel is a jQuery plugin for controlling a list of items in horizontal or vertical order.
+
+		http://sorgalla.com/jcarousel/ AND https://github.com/jsor/jcarousel
+
+	2. OpenLibrary
+
+		The cover art used in this script comes from the Open Library.
+
+		Open Library is an initiative of the Internet Archive, a 501(c)(3) non-profit.
+
+		http://openlibrary.org
+
+		Further information about the Open Library Covers API, including appropriate use, can be found at:
+
+		https://openlibrary.org/dev/docs/api/covers
+		
+	3. CSS and JS files
+	
+		To obtain the specific CSS and js files in use in the implmentation example cited above, visit:
+		
+		http://github.com/wadeguidry/Primo-Book-Cover-Carousel
+		
+		Note that the repository contains static versions of jQuery and the jcarousel jQuery plugin for convenience. Newer versions of these may well be available.
+
+In my environment, I have the script running once daily, and use Windows Server 2008 task scheduler to schedule the job.
+
+**************************************************/
+
 
 /* get the XML from the Analytics API and load into PHP var */
 
 /* in this case, a report of recently checked out and returned titles */
 
 
-/* function to prevent initial redirect on image query */
+
+/* START - prevent initial redirect on image queries sent to openlibrary.org */
 
 $context = stream_context_create(
     array(
@@ -27,50 +68,43 @@ $context = stream_context_create(
     )
 );
 
+/* END - prevent initial redirect on image queries sent to openlibrary.org */
 
-/* read the analytics report in */
+
+/* START - Read in the analytics report  */
 
 $ResumptionToken = "";
 
 while (($IsFinished != 'true') Or ($rowcount == 0)) {
+	
 	$report = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/analytics/reports?path=/shared/University%20of%20Puget%20Sound/Reports/Wade/trending_titles&limit=1000&apikey=[your api key]";
 	$xml = simplexml_load_file($report); 
 	$ResumptionToken = (string) $xml->QueryResult->ResumptionToken;
 	$IsFinished = (string) $xml->QueryResult->IsFinished;
-	
-	
+		
 	/* register the "rowset" namespace */
+	
 	$xml->registerXPathNamespace('rowset', 'urn:schemas-microsoft-com:xml-analysis:rowset');
 	
 	/* use xpath to get rows of interest */
 	
 	$result = $xml->xpath('/report/QueryResult/ResultXml/rowset:rowset/rowset:Row');
+	
+	/* using rowcount of 0 to repeat call until data is obtained is a low-tech way of addressing the issue that calls to the analytics API sometimes just fail;
+	   Since I know for certain that there should be data in the report */
+	
 	$rowcount = count($result);
 	}
-
-
-
 
 /* unique-ify the results , since the report is on items, but what we really want is titles, and there may be item duplicates */
 
 array_unique($result);
 
-/* function to sort array by specific key, call number in this case */
-
-/* function cmp($a, $b)
-    {
-        return strcmp($a->Column5, $b->Column5);
-    } */
-	
-/* use the function */
-
-/* usort($result, "cmp");	*/
-
-/* randomize the list */
+/* randomize the list, to keep it interesting */
 
 shuffle($result);
 
-/* start outputting desired results to new html file, which will replace the old file */
+/* start outputting desired results to new html file, which will replace the old file once the new file is built */
 
 $output = 'trending_c_new.html';
 
@@ -115,9 +149,9 @@ foreach ($result as $row) {
 	$loc = (string) $row->Column6;
 	$location = (string) $row->Column7;
 	
-	$isbn_array = explode (';' , $isbns);
+	/* create an array of the ISBN numbers, to check each number for the availability of cover art */
 	
-	/* if a title has book cover available on openlibrary, download the image and include the book in the carousel, otherwise, do not include the book */
+	$isbn_array = explode (';' , $isbns);
 	
 	/* go through each ISBN for a title to see if cover art exists for one of them */
 	
@@ -130,7 +164,9 @@ foreach ($result as $row) {
 		}
 	}
 	
-	/* if the cover art exists, then download the large size cover from openlibrary, do a bit of cleanup, and add the title to the html file */
+	/* if the cover art exists, then download the large size cover from openlibrary, do a bit of cleanup, and add the title to the html file
+	
+	   depending on the use case, you could instead include a default book cover for titles for which a cover is not available */
 
 	if ($size !== false) {
 	
